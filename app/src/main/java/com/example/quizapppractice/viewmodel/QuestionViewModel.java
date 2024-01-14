c27ccd8f2278f83583124485b6667ccf21c67581
Nomso <nomsounaogu@gmail.com>
2024-01-14 14:38:45 +0100

    Quiz App Practice Quiz List

---
+++ app/src/main/java/com/example/quizapppractice/viewmodel/QuestionViewModel.java
@@ -0,0 +1,41 @@
+package com.example.quizapppractice.viewmodel;
+
+import android.util.Log;
+
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.example.quizapppractice.Model.QuestionModel;
+import com.example.quizapppractice.repository.QuestionRepository;
+
+import java.util.List;
+
+public class QuestionViewModel extends ViewModel implements QuestionRepository.OnQuestionLoad {
+
+    private MutableLiveData<List<QuestionModel>> questionMutableLiveData;
+    private QuestionRepository repository;
+
+    public MutableLiveData<List<QuestionModel>> getQuestionMutableLiveData() {
+        return questionMutableLiveData;
+    }
+
+    public QuestionViewModel(){
+        questionMutableLiveData = new MutableLiveData<>();
+        repository = new QuestionRepository(this);
+    }
+
+    public void setQuizId(String quizId){
+        repository.setQuizId(quizId);
+    }
+
+    @Override
+    public void onLoad(List<QuestionModel> questionModels) {
+        questionMutableLiveData.setValue(questionModels);
+
+    }
+
+    @Override
+    public void onError(Exception e) {
+        Log.d("QuizError", "onError: " + e.getMessage());
+    }
+}
--- app/src/main/java/com/example/quizapppractice/views/DetailsFragment.java
+++ app/src/main/java/com/example/quizapppractice/views/DetailsFragment.java
@@ -2,65 +2,103 @@ package com.example.quizapppractice.views;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.example.quizapppractice.Model.QuizListModel;
 import com.example.quizapppractice.R;
+import com.example.quizapppractice.viewmodel.QuizListViewModel;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link DetailsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.List;
+
+
 public class DetailsFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public DetailsFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment DetailsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static DetailsFragment newInstance(String param1, String param2) {
-        DetailsFragment fragment = new DetailsFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
+    
+    private TextView title, difficulty, totalQuestions;
+    private Button startQuizBtn;
+    private NavController navController;
+    private int position;
+    private ProgressBar progressBar;
+    private QuizListViewModel viewModel;
+    private ImageView topicImage;
+    private String quizId;
+    private long totalQueCount;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_details, container, false);
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        viewModel = new ViewModelProvider(this, ViewModelProvider.AndroidViewModelFactory.
+                getInstance(getActivity().getApplication())).get(QuizListViewModel.class);
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        title = view.findViewById(R.id.detailFragmentTitle);
+        difficulty = view.findViewById(R.id.detailFragmentDifficulty);
+        totalQuestions = view.findViewById(R.id.detailFragmentQuestions);
+        startQuizBtn = view.findViewById(R.id.startQuizBtn);
+        progressBar = view.findViewById(R.id.detailProgressBar);
+        topicImage =view.findViewById(R.id.detailFragmentImage);
+        navController = Navigation.findNavController(view);
+
+        position = DetailsFragmentArgs.fromBundle(getArguments()).getPosition();
+
+        viewModel.getQuizListLiveData().observe(getViewLifecycleOwner(), new Observer<List<QuizListModel>>() {
+            @Override
+            public void onChanged(List<QuizListModel> quizListModels) {
+                QuizListModel quiz = quizListModels.get(position);
+                difficulty.setText(quiz.getDifficulty());
+                title.setText(quiz.getTitle());
+                totalQuestions.setText(String.valueOf(quiz.getQuestions()));
+                Glide.with(view).load(quiz.getImage()).into(topicImage);
+                Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        progressBar.setVisibility(View.GONE);
+                    }
+                }, 2000);
+
+                totalQueCount = quiz.getQuestions();
+                quizId = quiz.getQuizId();
+            }
+        });
+
+        startQuizBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DetailsFragmentDirections.ActionDetailsFragmentToQuizFragment action =
+                        DetailsFragmentDirections.actionDetailsFragmentToQuizFragment();
+
+                action.setQuizId(quizId);
+                action.setTotalQueCount(totalQueCount);
+                navController.navigate(action);
+            }
+        });
     }
 }
